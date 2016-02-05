f01347a8aedd18a4026eecaba3b541558863c57c
antokioukis <antokioukis@gmail.com>
2016-02-05 12:59:24 +0200

    5_2_16

--- evo_net.c
+++ evo_net.c
@@ -1,96 +1,134 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
-#define population 1000
-#define group_persons 100
+#define population 100000
+#define group_persons 1000
 #define genes_per_person 10
+#define max_gene_interaction 5
+#define max_gene_count 1000
 
-//kentriki idea exw kentrikos pinakas population grouparismenos o pli8ismos ana 100 se ka8e group
-//psaxnw se poio group anoikei o individual kai apo ekei akolou8w pointer pou paei se individual groups kai apo ekei akolou8w allo
-//pointer se personal genes
-//problima o pinakas indi genes dimiourgeite ka8e fora me ta idia random
+int debug_prints=0;
 
-typedef struct indi_genes{
-    int indi_genes [genes_per_person][2];
-}indi_genes;
+/*
+step 1: create array of floats nxn where n is genes per person (done but problem with srand doesnt create distinct random values FIXED)
+step 2: create array of genes per person (done rand works creates different sets of random values)
+step 3: create pointers to each of the above mentioned data structures (done)
+step 4: create person arrays each entry must have pointer to step 1 and step 2 (done)
+step 5: create population hash-table with pointers to each group_persons(done)
+step 6: check for errors in each malloc
+step 7: create the kill functions, you will specify how many people must die and will get removed. Printed before and after
+step 8: what about the 2 other matrixes?
+*/
 
-typedef struct indi_groups{
-    int person_id;
-    indi_genes *indi_genes;
-    struct indi_groups *next;
-}indi_groups;
+/*Logic of program
+    Create array named population which points to each each group of persons. In order for the program to be scalable each bucket contains N persons
+    Each bucket points to an array where all the people from that group have 2 pointers: 1 pointer to their personal gene counts and 1 pointer to their
+    personal gene interactions
+
+                min<id<max           personal_id
+    population-------------->bucket---------------->personal reconds
+
+    Basically population is a hash table
+*/
+
+
+typedef struct gene_interaction_matrix{
+    float gene_inter_matrix[genes_per_person][genes_per_person];
+}gene_inter_matrix;
+
+typedef struct individual_gene_count{
+    int indi_gene_array[genes_per_person][2];
+}indi_gene_count;
+
+typedef struct group_persons_array{
+    int personal_id;
+    indi_gene_count *ptr_to_genes_count;
+    gene_inter_matrix *gene_dependancies;    //need a better name
+}group_persons_array;
 
 typedef struct population_table{
-    int value [population/group_persons][2];
+    int max_id [population/group_persons];
+    group_persons_array *ptr_to_group_persons;
 }population_table;
 
-indi_genes *create_indi_genes(){
+gene_inter_matrix* initialize_gene_inter_matrix(){
+    int i,j;
+    gene_inter_matrix *ptr_to_gene_inter_matrix;
+    ptr_to_gene_inter_matrix=(gene_inter_matrix*)malloc(sizeof(gene_inter_matrix));
 
-    int i;
-    indi_genes *array_of_indi;
-
-    time_t t;
-    srand((unsigned) time(&t));
-
-    array_of_indi = (indi_genes *)malloc(sizeof(indi_genes));
 
     for (i=0;i<genes_per_person;i++){
-        array_of_indi->indi_genes[i][0]=i;
-        array_of_indi->indi_genes[i][1]=rand()%500;
+        for (j=0;j<genes_per_person;j++){
+           ptr_to_gene_inter_matrix->gene_inter_matrix[i][j]=(((float)rand()/(float)(RAND_MAX)) * max_gene_interaction);
+          }
     }
+    /*
+    printf("Pinakas Dependencies Sigkekrimenou Atomou\n");
+    for (i=0;i<genes_per_person;i++){
+        for (j=0;j<genes_per_person;j++){
+           printf(" %f ",ptr_to_gene_inter_matrix->gene_inter_matrix[i][j]);
+        }
+        printf("\n");
+    }
+*/
 
-//   for (i=0;i<genes_per_person;i++){
-//       printf("%d",array_of_indi->indi_genes[i][0]);
-//       printf("%d",array_of_indi->indi_genes[i][1]);
-//       printf("\n");
-//    }
-
-    return array_of_indi;
+    return ptr_to_gene_inter_matrix;
 
 }
 
-void push_indi_group(indi_groups * head, int val) {
-    int j;
-    indi_groups * current = head;
-    while (current->next != NULL) {
-        current = current->next;
-    }
-
-    /* now we can add a new variable */
-    current->next = (indi_groups *)malloc(sizeof(indi_groups));
-    current->person_id=val;
-    current->next->indi_genes = create_indi_genes();
-
-//    for (j=0;j<genes_per_person;j++){
-//            printf("%d ",current->next->indi_genes->indi_genes[j][1]);
-//    }
-    printf("\n");
-    current->next->next = NULL;
-}
-
-indi_groups *create_indi_groups(){
+indi_gene_count* initialize_indi_gene_count(){
     int i;
-    indi_groups *array_of_groups = NULL;
+    indi_gene_count *ptr_to_indi_gene_count;
+    ptr_to_indi_gene_count = (indi_gene_count *)malloc(sizeof(indi_gene_count));
 
-    array_of_groups = (indi_groups *)malloc(sizeof(indi_groups));
-
-    if (array_of_groups == NULL) {
-        return NULL;
+    for (i=0;i<genes_per_person;i++){
+        ptr_to_indi_gene_count->indi_gene_array[i][0]=i;
+        ptr_to_indi_gene_count->indi_gene_array[i][1]=rand()%500;
     }
 
-    array_of_groups->indi_genes = create_indi_genes();
-    array_of_groups->next = NULL;
-    array_of_groups->person_id=0;
-
-
-    for (i=1;i<group_persons;i++){
-        push_indi_group(array_of_groups,i);
+/*
+    for (i=0;i<genes_per_person;i++){
+       printf("Gene No %d ",ptr_to_indi_gene_count->indi_gene_array[i][0]);
+       printf("Gene Count %d ",ptr_to_indi_gene_count->indi_gene_array[i][1]);
+       printf("\n");
     }
 
-
-    return array_of_groups;
-
+*/
+    return ptr_to_indi_gene_count;
 }
+
+group_persons_array* initialize_group_persons_array(){
+    int i,j,k,l;
+    group_persons_array *ptr_to_group_persons_array;
+
+    ptr_to_group_persons_array = (group_persons_array *)malloc(sizeof(group_persons_array));
+
+    for (i=0;i<group_persons;i++){
+        ptr_to_group_persons_array->personal_id=i;
+        ptr_to_group_persons_array->ptr_to_genes_count=initialize_indi_gene_count();
+        ptr_to_group_persons_array->gene_dependancies=initialize_gene_inter_matrix();
+        if(debug_prints){
+            printf("Gia to Atomo %d\n", ptr_to_group_persons_array->personal_id);
+            for (j=0;j<genes_per_person;j++){
+                printf("Gene No %d ", ptr_to_group_persons_array->ptr_to_genes_count->indi_gene_array[j][0]);
+                printf("Gene Count %d ", ptr_to_group_persons_array->ptr_to_genes_count->indi_gene_array[j][1]);
+                printf("\n");
+            }
+
+            printf("Pinakas Dependencies Sigkekrimenou Atomou\n");
+            for (k=0;k<genes_per_person;k++){
+                for (l=0;l<genes_per_person;l++){
+                    printf(" %f ",ptr_to_group_persons_array->gene_dependancies->gene_inter_matrix[k][l]);
+                }
+                printf("\n");
+            }
+            printf("\n");
+            }
+    }
+    return ptr_to_group_persons_array;
+}
+
 
 void create_population(){
     int i;
@@ -98,34 +136,37 @@ void create_population(){
     poptable = (population_table *)malloc(sizeof(population_table));
 
     for (i=0;i<population/group_persons;i++){
-        poptable->value[i][0]=i*100;
-        poptable->value[i][1]=create_indi_groups();
+        if(debug_prints) printf("Sto group me minimum id %d kai max id %d\n", poptable->max_id[i],poptable->max_id[i]+group_persons-1);
+        poptable->max_id[i]=i*group_persons;
+        poptable->ptr_to_group_persons=initialize_group_persons_array(debug_prints);
     }
-//    for (i=0;i<population/group_persons;i++){
-//        printf("mexri ari8mo: %d\n",poptable-> value[i][0]);
-//    }
 }
 
-int main (void){
-    int i,j;
-    indi_groups *array_of_groups;
-    indi_groups *temp;
+void initialization(){
+    srand(time(NULL));
 
-//    create_indi_genes();
-//    create_indi_genes();
+//    gene_inter_matrix *i;
+//    gene_inter_matrix *j;
+//
+//    indi_gene_count *k;
+//    indi_gene_count *l;
+
+//    group_persons_array *a;
+//    group_persons_array *b;
+
+//    i=initialize_gene_inter_matrix();
+//    j=initialize_gene_inter_matrix();
+//
+//    k=initialize_indi_gene_count();
+//    l=initialize_indi_gene_count();
+
+//    a=initialize_group_persons_array();
+//    b=initialize_group_persons_array();
 
     create_population();
+}
 
-//    array_of_groups=create_indi_groups();
-//    temp=array_of_groups;
-//
-//    for (i=0;i<group_persons;i++){
-//        printf("%d ",temp->person_id);
-//        for (j=0;j<genes_per_person;j++){
-//            printf("%d ",temp->indi_genes->indi_genes[j][1]);
-//        }
-//        printf("\n");
-//        temp=temp->next;
-//    }
-
+int main (int argc, char *argv[]){
+    initialization();
+    return 0;
 }
