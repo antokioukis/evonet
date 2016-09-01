ad0acbfc73b5f1b391cda17332540b1e2d3032e4
Antonios Kioukis <antokioukis@gmail.com>
2016-09-01 18:00:14 +0300

    data mine R + anafora

---
+++ ptixiaki.R
@@ -0,0 +1,28 @@
+#fitness images export
+setwd("~/Dropbox/Sxoli/ptixiaki/evo3/")
+
+my_fitness=read.table("fitness.txt")
+
+fitness_of_generation<-as.vector(unlist(my_fitness[4]))
+num_of_generation<-as.vector(as.numeric(unlist(my_fitness[1])))
+
+png(filename="~/Dropbox/Sxoli/ptixiaki/evo3/fit10.png")
+plot(num_of_generation,fitness_of_generation,main="s^2 = 10",type="l",xlab="Generation Number",ylab="Generation Fitness")
+dev.off()
+
+#matrix interaction export
+
+interaction_matrixes=read.table("matrix.txt")
+interaction_matrixes[interaction_matrixes == 0] <- NA
+
+atomo <- split(interaction_matrixes,rep(1:1000,each=10))
+
+for(i in 1:1000){
+  kenes_stiles[i]<-sum(is.na.data.frame(atomo[i]))/10
+  sum<-0
+}
+
+png(filename="~/Dropbox/Sxoli/ptixiaki/evo3/non_interaction.png")
+hist(kenes_stiles,main="Non Interacting Genes",xlab="Number of non interacting Genes",ylab="Number of Individuals")
+
+dev.off()
