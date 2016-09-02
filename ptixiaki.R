c99842106a1909d60652be38438e8125fdb15c75
pavlos <pavlos@zoya.imbb.forth.gr>
2016-09-02 13:59:56 +0300

    swstos R kodikas... eprepe na se eixa kopsei sto mathima

--- ptixiaki.R
+++ ptixiaki.R
@@ -12,11 +12,25 @@ dev.off()
 
 #matrix interaction export
 
-interaction_matrixes=read.table("matrix.txt")
-interaction_matrixes[interaction_matrixes == 0] <- NA
+interaction_matrixes=read.table("newMatrix.txt")
 
-atomo <- split(interaction_matrixes,rep(1:1000,each=10))
+dim(interaction_matrixes)
 
+
+##interaction_matrixes[interaction_matrixes == 0] <- NA
+auxMatrix <- interaction_matrixes; #[interaction_matrixes == 0] <- NA
+auxMatrix[auxMatrix == 0] <- NA
+
+auxMatrix[[21000]]
+
+atomo <- split(interaction_matrixes,rep(1:21000,each=10))
+auxAtomo <- split(auxMatrix,rep(1:21000,each=10))
+
+findNAs <- function(element){
+    apply(element, 2, function(x){ sum(is.na(x))})
+}
+
+length(atomo)
 for(i in 1:1000){
   kenes_stiles[i]<-sum(is.na.data.frame(atomo[i]))/10
   sum<-0
