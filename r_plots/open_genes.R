3063b3d4b04994933155b2dcd339d6ac7c071add
Antonios Kioukis <antokioukis@gmail.com>
2019-09-05 09:34:13 +0000

    open_genes

--- r_plots/open_genes.R
+++ r_plots/open_genes.R
@@ -1,5 +1,24 @@
 rm(list=ls())
 
+read_fitness <- function(fdirName,curDir,n) {
+  final_fitness<-matrix(nrow=11,ncol=100)
+  differences<-c(1.000000,0.818731,0.753638,0.707222,0.670320,0.639407,0.612689,0.589105,0.567971,0.548812,0.531286)
+  rev_differences <- rev(differences)
+  for(i in 1:n) {
+      print(i)
+      dirName <- paste(curDir,"/",fdirName, i, sep="")
+      setwd(dirName)
+      fitness<-read.table("fitnesss.txt")
+      for (j in 1:(length(rev_differences)-1)){
+        min_index <- min(which(fitness>rev_differences[j]))
+        final_fitness[j,i] <- min_index
+      }
+      final_fitness[11,i] <- nrow(fitness)
+  }
+  return (final_fitness)
+}
+
+
 read_data <- function(fdirName,curDir,n) {
   final_open<-matrix(nrow=500,ncol=100)
   vec<-c()
@@ -20,25 +39,25 @@ read_data <- function(fdirName,curDir,n) {
   for(i in 1:n) {
       print(i)
     #dirName <- fdirName
-      dirName <- paste(curDir,"/",fdirName,"run.", i, sep="")
+      dirName <- paste(curDir,"/",fdirName, i, sep="")
       if(!dir.exists(file.path(dirName))){next}
       else{
           setwd(dirName)
           open_genes<-read.table("open_genes.txt")
           fitness<-read.table("fitnesss.txt")
           ##print(fitness[nrow(fitness),4])
-          print(length(unique(fitness[,4])))
+          print(length(unique(fitness[,1])))
           #print(unique(fitness[,4]))
-          if(fitness[nrow(fitness),4]<0.80){
-              next
-          }
+          #if(fitness[nrow(fitness),1]<0.80){
+          #    next
+          #}
           ##edw exeis to meso oro open genes per generation
       open_genes_mean<-rowMeans(open_genes)
       #final_open[,i]<-open_genes_mean
       #plot(open_genes_mean,type="p")
       #print(open_genes_mean)
-          temp_data<-cbind(open_genes_mean,fitness[,4])
-          print(temp_data)
+      temp_data<-cbind(open_genes_mean,fitness[,1])
+      print(temp_data)
       colnames(temp_data) <- NULL
       for (j in 1:nrow(temp_data)){
         if(temp_data[j,2]>differences[1]){
@@ -104,11 +123,12 @@ read_data <- function(fdirName,curDir,n) {
 }
 
 n<-100
-curDir<-"~/evo3"
-fdirName<-"500selection1rr0.05."
+curDir<-"/home/antonios/Downloads/evonet/"
+fdirName<-"500selection.0.05.run."
 #final_open<-matrix(nrow=100,ncol=1000)
 
 output_data<-read_data(fdirName,curDir,n)
+'''
 plot(colMeans(output_data, na.rm=TRUE))
 
 output_data
@@ -118,30 +138,53 @@ output_data2<-output_data
 output_data
 keep_vec<-c()
 for (i in 1:nrow(output_data2)){
-
   print(sum(output_data2[i,]))
-  if(sum(output_data2[i,])!=0){ keep_vec<-c(keep_vec,i)}
+  if(sum(output_data2[i,])!=0){
+    keep_vec<-c(keep_vec,i)
+  }
 }
 
-output_data2<-output_data2[keep_vec,]
 
+output_data2<-output_data2[keep_vec,]
+'''
 y <- colMeans(output_data, na.rm=TRUE)
+trans_out_data <- melt(t(output_data))
+y[is.nan(y)] <- 0
 x <- differences<-c(1.000000,0.818731,0.753638,0.707222,0.670320,0.639407,0.612689,0.589105,0.567971,0.548812,0.531286)
 
 output_data
-
+fit_data <- read_fitness(fdirName,curDir,n)
+fit_data[10,64] <- 500 # den 3erw giati alla to run 64 den brike kati, logika ekane megalo alma apo to ena sto allo difference
+fit_data <- data.frame(fit_data)
+# mesos oros genias pou epiase auto to fitness level
+trans_fit_data <- melt(t(fit_data))
+rev_differences<-rev(c(1.000000,0.818731,0.753638,0.707222,0.670320,0.639407,0.612689,0.589105,0.567971,0.548812,0.531286))
+replace_columns <- rep(rev_differences, each=100)
+trans_fit_data$Var2 <- replace_columns
 library(ggplot2)
+
+g1 <- ggplot(trans_fit_data, aes(x=Var2,y=value, group=Var2)) + geom_boxplot()
+g2 <- g1 + geom_point(aes(y=rep(y, each=100)),col='red') +
+  scale_y_continuous(
+    "mpg (US)", 
+    sec.axis = sec_axis(~ . * 0.00001, name = "mpg (UK)")
+  )
+
+'''
 data <- data.frame(opengenes=x, fitbin=y)
-ggplot(data, aes(x=opengenes, y=fitbin)) + geom_point(shape=1)
+ggplot(data, aes(x=opengenes, y=fitbin)) + geom_point(shape=1, col='red')
 
 
 plot_vec<-c(colMeans(output_data2))
 
 plot(seq(0, 1, 0.1),plot_vec,ylab="Mean open genes",xlab="Generation Fitness")
-
+'''
 #h logiki einai:
 # diavase to open_genes.txt kai to fitness.txt
 # enwse ta 2 txt giati panta 8a exoun to idio length logw oti anikoun sto idio run
 # katigoroiopoihse tis grammes twn combined data se bins analoga me to generation fitness
 # epanelabe tin diadikasia gia ola ta runs
 # bgale to colmean kai plottare to
+
+# to parataw gia twra giati prepei na doulepsw kai ligo gia ton ilia
+# h teleutaia idea einai na na exeis grouped barplot 2 koutia ana fitness level to ena na deixnei to fitness kai to allo na deixnei ta open genes
